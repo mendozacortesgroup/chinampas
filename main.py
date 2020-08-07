2c7cdc54eae6f032576d9b222b667d6868603209
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2020-08-07 07:53:33 -0400

    self.fill() returns all activations print(self) now makes a copy of external activations

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ class plants():
     def __init__(self, cost=0,external=3):
         if external <3:
             return None
-        self.n = external-3
-        self.cost = cost
-        self.chinampa = {0:[complex(0,0),complex(1,0),complex(2,0)]}
-        self.top =  complex(2,2)
-        self.h = 2
-            
+        self.n = external-3  # we take 3 for pyramid(3) 
+        self.cost = cost  # to allow other cost
+        self.chinampa = {0:[complex(0,0),complex(1,0),complex(2,0)]}  # pyramid3
+        self.top =  complex(2,2)  # auxiliary pointer
+        self.h = 2  # height
+        self.all = {}  # all activated
+        
+    def fill(self):
+        self.__str__()
+        return self.all
+    
     def gl(self,degree = 0):
         self.chinampa[self.h-degree] = self.chinampa.get(self.h-degree,[]) + [self.top-1 -degree*(1j+1)]
         self.h = self.h+1
@@ -26,7 +31,9 @@ class plants():
         self.top = self.top +1+1j
         
     def __str__(self):
-        externals = self.chinampa
+        pdb.set_trace()
+        self.all = self.chinampa.copy()
+        externals = self.all
         for i in range(self.h):
             listOfExternals = externals.get(i,[])
             numberAtLeveli = len(listOfExternals)
@@ -35,11 +42,10 @@ class plants():
                 listOfExternals = sorted( listOfExternals, key = lambda x: x.real)                
                 initial = listOfExternals[0]
                 for element in listOfExternals[1:]:
-                    pdb.set_trace()
                     if element == initial+1:
-                        self.chinampa[i+1] = self.chinampa.get(i+1,[])+[element+1j]
+                        self.all[i+1] = self.all.get(i+1,[])+[element+1j]
                         initial = element
-        return str(1)
+        return str('output in self.all')
             
             
    
