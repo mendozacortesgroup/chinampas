5e569c5e76652d36323e0d3140ac36498ebf12f3
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2020-11-10 13:45:56 -0500

    renamed variables for easier comprehension

--- main.py
+++ main.py
@@ -4,6 +4,73 @@ Created on Sun Aug  2 16:29:07 2020
 
 @author: FSU
 """
+
+
+ #The following program is not efficient   
+ # it is mean to find the number of solutions to systemsw of equations
+from scipy.special import comb
+def doublehandle(n):
+    oneHandle = [1 for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n) for A in range(a+1,c) for d in range(c+1,n+1) ]
+    sol = [d-A-1 for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n) for A in range(a+1,c) for d in range(c+1,n+1) ]
+    com = comb(n, 4, exact=True)
+    return n, com, sum(oneHandle), sum(sol), sum(sol)/n, sum(sol)/com, sum(sol)/sum(oneHandle),sum(sol)/(sum(oneHandle)*n),sum(sol)/(sum(oneHandle)*com),sum(sol)/(n*sum(oneHandle)*com)  
+
+
+print('n (n choose 4) Yw1h Yw2h Yw2h/n Yw2h/(n choose 4) Yw2h/yw1h')
+for i in range(4,10):
+    print(doublehandle(i))
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+
+
+           
+import matplotlib.pyplot as plt
+
+plt.plot([4,5,6,7,8,9], [1/1,10/5,50/15,175/35,490/70,1176/126], linewidth=2)
+plt.xlabel(' n ')
+plt.ylabel('O(doubleHandle)/O(line) ')
+plt.title("O(doubleHandle)/O(line) as a function of 'n' ")
+plt.show()    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from copy import deepcopy
 import pdb
 import pprint
@@ -114,80 +181,6 @@ def cost_Zero(n=3, printed = False):
 #assert len(first) == 5
 #print('test with all plants on 5 vertex passed')
 #[pprint.pprint(elem.fill(),indent=4,width=1) for elem in first]
-    
- #The following program is not efficient   
- # it is mean to find the number of solutions to systemsw of equations
-from scipy.special import comb
-def tester(n):
-    easy = [1 for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n) for A in range(a+1,c) for d in range(c+1,n+1) ]
-    sol = [d-A-1 for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n) for A in range(a+1,c) for d in range(c+1,n+1) ]
-    com = comb(n, 4, exact=True)
-    return n, com, sum(easy), sum(sol), sum(sol)/n, sum(sol)/com, sum(sol)/sum(easy),sum(sol)/(sum(easy)*n),sum(sol)/(sum(easy)*com),sum(sol)/(n*sum(easy)*com)  
-
-
-print('n (n choose 4) Yw1h Yw2h Yw2h/n Yw2h/(n choose 4) Yw2h/yw1h')
-for i in range(4,10):
-    print(tester(i))
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-
-
-
-
-# generate points used to plot
-x_plot = np.linspace(0, 10,11)
-
-# generate points and keep a subset of them
-x = np.array([1,7,27])
-y = np.array([1,10,50])
-
-# create matrix versions of these arrays
-X = x[:, np.newaxis]
-X_plot = x_plot[:, np.newaxis]
-
-colors = ['teal', 'yellowgreen', 'gold']
-lw = 2
-#plt.plot(x, y, color='cornflowerblue', linewidth=lw,
-#         label="ground truth")
-#plt.scatter(x, y, color='navy', s=30, marker='o', label="training points")
-
-for count, degree in enumerate([2, 3, 4]):
-    model = make_pipeline(PolynomialFeatures(degree), Ridge())
-    model.fit(X, y)
-    y_plot = model.predict(X_plot)
-    plt.plot(x_plot, y_plot, color=colors[count], linewidth=lw,
-             label=f"degree {degree}, coeff at 0, 1,  2 {y_plot}")
-
-plt.legend(loc='lower left')
-
-plt.show()
-           
-import matplotlib.pyplot as plt
-
-plt.plot([4,5,6,7,8,9], [1/1,10/5,50/15,175/35,490/70,1176/126], linewidth=2)
-plt.show()    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
