d74785ce14455916c089ebe1b7add337ffa4a0bb
Eric Rubiel <eric.rubiel@u.northwestern.edu>
2021-03-28 13:39:00 -0400

    cleaning

--- main.py
+++ main.py
@@ -2,83 +2,11 @@
 """
 Created on Sun Aug  2 16:29:07 2020
 
-@author: FSU
+@author: Eric Dolores. Antonio Arciniega
 """
-
-
- #The following program is not efficient   
- # it is mean to find the number of solutions to systemsw of equations
-from scipy.special import comb
-
-
-def doublehandleJoined(n):# a<B,B2<b<C,C2<c 
-    sol = [(b-a-1)**2*(c-b-1)**2 for a in range(1,n-3) for b in range(a+1, n-1) for c in range(b+1, n+1) ]
-    return  sum(sol)#, sum(sol)/n, sum(sol)/com, sum(sol)/sum(oneHandle),sum(sol)/(sum(oneHandle)*n),sum(sol)/(sum(oneHandle)*com),sum(sol)/(n*sum(oneHandle)*com)  
-
-
-
-
-def rplot(M):
-    dh = [doublehandleJoined(n) for n in range(5,M)]
-    N = [v for v in range(5,M)]
-        
-    cub = [doublehandleJoined(n)/(n*comb(n, 5, exact=True)) for n in range(5,M)]
-    print(f'points {N}\n  \n double joined handle {dh}')    
-    import matplotlib.pyplot as plt
-    
-    
-    plt.plot( N,cub, linewidth=2)
-    plt.xlabel(' n ')
-    plt.ylabel(f'O(double joined Handle))/n*comb ')
-    plt.title(f"O(duuble joined handle)/n*comb as a function of 'n' ")
-    plt.show()    
-    #plt.plot()
-rplot(40)
-
-### results double joined handle [1, 10, 53, 200, 606, 1572, 3630, 7656, 15015, 27742, 48763, 82160, 133484, 210120, 321708, 480624, 702525, 1006962, 1418065, 1965304, 2684330, 3617900, 4816890, 6341400, 8261955, 10660806, 13633335, 17289568, 21755800, 27176336, 33715352, 41558880, 50916921, 62025690, 75149997]
-
-    
-    
-def doublehandleSeparated(n):# a<b<c<d x<B<c b<C<d
-    sol = [d-A-1 for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n) for A in range(a+1,c) for d in range(c+1,n+1) ]
-    return  sum(sol)#, sum(sol)/n, sum(sol)/com, sum(sol)/sum(oneHandle),sum(sol)/(sum(oneHandle)*n),sum(sol)/(sum(oneHandle)*com),sum(sol)/(n*sum(oneHandle)*com)  
-
-def triplehandle(n):
-    Handle = [(c-b-1)*(B-a-1)*(d-B-1) 
-             for a in range(1,n-2) for b in range(a+1, n-1) for c in range(b+1, n)  for d in range(c+1, n+1) for B in range(b+1, c)]
-    return sum(Handle)    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from copy import deepcopy
-import pdb
+#import pdb
 import pprint
-
 
 class plants():
     def __init__(self, external=3,cost=0):
@@ -168,6 +96,7 @@ def poli(plant,garden, n, digging = True):
 
 def cost_Zero(n=3, printed = False):
     # this returns a list of all cascades with n external activations and cost 0
+    # each cascade is a list of activated vertex
     pyramid = plants()
     ext = n-3
     garden = []
@@ -180,11 +109,12 @@ def cost_Zero(n=3, printed = False):
     if printed:
         [pprint.pprint(elem.fill(), indent=4, width=1) for elem in garden]
     return garden        
-    
-#first = cost_Zero(4, True)
-#assert len(first) == 5
-#print('test with all plants on 5 vertex passed')
-#[pprint.pprint(elem.fill(),indent=4,width=1) for elem in first]
+print("This program generates activation graphs")
+print("Each activation graph is a list of activated vertex")    
+first = cost_Zero(4, True)
+assert len(first) == 5
+print('test with all plants on 5 vertex passed')
+[pprint.pprint(elem.fill(),indent=4,width=1) for elem in first]
     
     
     
